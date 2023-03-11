a1d8cbef7fd4d907f9ddd61e322e0080611f2a2c
Bedirhan <bedirhansiyam@gmail.com>
2023-03-11 13:11:22 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿namespace ForLoop;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i < count; i++)
+        {
+            if(i % 2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /* Output :
+           1
+           3
+           5
+           .
+           .        */
+
+        //---------------------   
+
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if( i % 2 == 1)
+            {
+                oddSum += i;
+            }
+            else
+            {
+                evenSum += i;
+            }            
+        }
+        Console.WriteLine("Even sum = " + evenSum);
+        Console.WriteLine("Odd sum = " + oddSum);
+
+        // break  
+        for (int i = 1; i < 6; i++)
+        {
+            if(i == 4)
+            break;
+            Console.WriteLine(i);
+        } 
+        /*  Output:
+            1
+            2
+            3     */
+
+
+        // continue
+        for (int i = 1; i < 6; i++)
+        {
+            if(i == 4)
+            continue;
+            Console.WriteLine(i);
+        } 
+        /*  Output:
+            1
+            2
+            3
+            5
+            6     */
+    }
+}
